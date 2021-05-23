3d033f87203a7181218426edf596256018489cb1
Andrés Velasco <velasco.andrs@gmail.com>
2021-05-22 22:32:12 -0500

    override publications findAll

--- src/sis-modules/publication/publication.controller.ts
+++ src/sis-modules/publication/publication.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 import { CrudController, CrudOptions } from '@nest-excalibur/common-api/lib';
 
@@ -6,27 +6,34 @@ import { PublicationEntity } from './publication.entity';
 import { PublicationService } from './publication.service';
 import { PublicationCreateDto } from './dtos/publication-create.dto';
 import { PublicationUpdateDto } from './dtos/publication-update.dto';
+import { ObjectLiteral } from 'typeorm';
 
 
 const options: CrudOptions = {
-    dtoConfig: {
-        createDtoType: PublicationCreateDto,
-        updateDtoType: PublicationUpdateDto,
-    },
-    useMongo: true,
-    enableErrorMessages: true,
+  dtoConfig: {
+    createDtoType: PublicationCreateDto,
+    updateDtoType: PublicationUpdateDto,
+  },
+  useMongo: true,
+  enableErrorMessages: true,
 };
 
 
 @Controller('publication')
 export class PublicationController
-    extends CrudController<PublicationEntity>(options)
-{
-    constructor(
-        private readonly publicationService: PublicationService,
-    ) {
-        super(
-          publicationService,
-        );
-    }
+  extends CrudController<PublicationEntity>(options) {
+  constructor(
+    private readonly publicationService: PublicationService,
+  ) {
+    super(
+      publicationService,
+    );
+  }
+
+  @Get()
+  findAll(
+    @Query() searchCriteria: ObjectLiteral = {}): any {
+    return this.publicationService.filterByLocation(searchCriteria);
+  }
+
 }
--- src/sis-modules/publication/publication.service.ts
+++ src/sis-modules/publication/publication.service.ts
@@ -3,24 +3,64 @@ import { PublicationEntity } from './publication.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { AbstractMongoService } from '@nest-excalibur/common-api/lib';
+import { query } from 'express';
 
 @Injectable()
 export class PublicationService
-    extends AbstractMongoService<PublicationEntity>{
+  extends AbstractMongoService<PublicationEntity> {
 
-    constructor(
-        @InjectRepository(PublicationEntity, 'mongo_conn')
-        private readonly publicationRepository: MongoRepository<PublicationEntity>,
-    ) {
-        super(
-            publicationRepository,
-            {
-                fieldOrSpec: { location: '2dsphere' },
-                options: {
-                    min: -180,
-                    max: 180,
-                },
-            },
-        );
-    }
+  constructor(
+    @InjectRepository(PublicationEntity, 'mongo_conn')
+    private readonly publicationRepository: MongoRepository<PublicationEntity>,
+  ) {
+    super(
+      publicationRepository,
+      {
+        fieldOrSpec: { location: '2dsphere' },
+        options: {
+          min: -180,
+          max: 180,
+        },
+      },
+    );
+  }
+
+  async filterByLocation(params: Record<string, any>, location?: [number, number], enable = 1) {
+    const { skip, take } = JSON.parse(params?.query) ?? {skip: 0, take: 10};
+    const cursor = this.publicationRepository.aggregate(
+      [
+        {
+          $match: {
+            // Params
+            enable,
+            ...JSON.parse(params?.query)?.where ?? {},
+          },
+        },
+        {
+          // Joins
+          $lookup: {
+            from: 'property_picture',
+            localField: '_id',
+            foreignField: 'publicationId',
+            as: 'pictures',
+          },
+        },
+        {
+          $facet: {
+            data: [
+              { $skip: skip },
+              { $limit: take },
+            ],
+            pageInfo: [
+              { $group: { _id: null, count: { $sum: 1 } } },
+            ],
+          },
+        },
+      ],
+    );
+    return cursor
+      .skip(skip ?? 0)
+      .limit(take ?? 10)
+      .toArray();
+  }
 }
